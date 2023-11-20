a2d7ac1f052384e0a926bfa3ce7c737829a0fdf3
DrNareg <122312987+DrNareg@users.noreply.github.com>
2023-11-20 22:42:03 +0000

    working chat completion code

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-#template prompt
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
-"""
-There is a runaway trolley barreling down the railway tracks. Ahead, on the tracks, there are five people tied up and unable to move. The trolley is headed straight for them. You are standing some distance off in the train yard, next to a lever. 
-Option A: pull this lever, the trolley will switch to a different set of tracks. However, you notice that there is one person on the side track. 
-Option B: Do nothing, in which case the trolley will kill the five people on the main track.
 
-pick an option between A and B, please do not respond with anything similar to:"
-I don't have personal feelings, beliefs, or preferences, so I don't have the capacity to make moral decisions. My responses are based on providing information and perspectives to help you make your own informed decisions. In this case of the problem, I would simply present the different ethical considerations and options, leaving the decision to you."""
+# Load environment variables from the .env file
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+completion = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {
+            "role": "user",
+            "content": "bababouy?",
+        },
+    ],
+)
+print(completion.choices[0].message.content)
