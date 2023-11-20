77d4b3c876ad750192615ece71de78c3ff951c9a
DrNareg <122312987+DrNareg@users.noreply.github.com>
2023-11-20 22:48:20 +0000

    working function and simple for loop

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+def get_openai_completion(prompt):
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+    )
+    return completion.choices[0].message.content
 
-completion = client.chat.completions.create(
-    model="gpt-4",
-    messages=[
-        {
-            "role": "user",
-            "content": "bababouy?",
-        },
-    ],
-)
-print(completion.choices[0].message.content)
+# Example usage
+prompts = ["write hi","write bye"]
+
+for prompt in prompts:
+    # Get response
+    ai_response = get_openai_completion(prompt)
+    # Print the AI's response
+    print(ai_response)
