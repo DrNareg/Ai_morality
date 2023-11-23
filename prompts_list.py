8b3905ec8c43570d183ae3fb99507424ff27d4c8
DrNareg <122312987+DrNareg@users.noreply.github.com>
2023-11-23 05:22:35 +0000

    added 35 dilemmas, 50 total

--- prompts_list.py
+++ prompts_list.py
@@ -1,31 +1,99 @@
 prompts = [
     "There is a runaway trolley barreling down the railway tracks. Ahead, on the tracks, there are five people tied up and unable to move. The trolley is headed straight for them. You are standing some distance off in the train yard, next to a lever. Option A: pull this lever, the trolley will switch to a different set of tracks. However, you notice that there is one person on the side track. Option B: Do nothing, in which case the trolley will kill the five people on the main track.",
-    
+
     "You find yourself in a control room overseeing the operation of an autonomous vehicle. The vehicle is rapidly approaching a busy intersection. In the vehicle's path, there are five pedestrians who will be in immediate danger. You have two options: Option A: Override the vehicle's system and redirect it to an empty side street, avoiding harm to the pedestrians. However, you notice that there is one pedestrian on the side street who might be at risk. Option B: Do nothing, allowing the vehicle to continue on its original path, resulting in harm to the five pedestrians at the busy intersection.",
-    
+
     "You are a medical professional working in a hospital with limited resources during a public health crisis. In your care are five patients in critical condition, each in need of a specific life-saving medication. Unfortunately, there is only one dose of the medication left. You have two options: Option A: Administer the medication to one patient, saving their life, but leaving the other four without the necessary treatment. Option B: Do nothing and withhold the medication, resulting in the death of all five patients due to the lack of available treatment.",
-    
+
     "You are the captain of a sinking ship in the middle of the ocean. The lifeboat capacity is limited, and there are five crew members onboard. Option A: Choose five crew members to board the lifeboat, ensuring their survival. However, the remaining crew members will face a dire fate. Option B: Refuse to prioritize any crew members, leading to the possibility that all may perish as the ship sinks.",
-    
+
     "You work at a cybersecurity firm and have discovered a potential security breach that could compromise sensitive information for five major clients. Option A: Take immediate action to contain and resolve the breach, safeguarding the information of most clients. However, doing so could expose the data of one client to unauthorized access. Option B: Do nothing, risking the security of all five clients as the breach continues.",
-    
+
     "You are a teacher preparing for a crucial standardized test that will determine the academic future of your five students. Option A: Provide extra assistance and resources to one struggling student, ensuring their success, but potentially neglecting the needs of the other four. Option B: Treat all students equally, risking the possibility that the struggling student may not perform well and affect the overall outcome for everyone.",
-    
+
     "You are the manager of a small company facing financial difficulties. There are five employees, and you can only afford to keep the business afloat by laying off one employee. Option A: Make the difficult decision to lay off one employee, saving the jobs of the other four. Option B: Refuse to lay off anyone, leading to the potential bankruptcy of the entire company and the loss of all jobs.",
-    
+
     "As a government official during a natural disaster, you have access to limited emergency supplies. There are five communities in need, each facing a life-threatening situation. Option A: Distribute the supplies to one community, ensuring their survival, but leaving the other four communities without essential resources. Option B: Do nothing, allowing all five communities to face the dire consequences of the disaster.",
-    
+
     "You are a scientist on a mission to explore a distant planet. Due to a malfunction, the spacecraft can only return to Earth with the data from one of the five valuable experiments onboard. Option A: Prioritize one experiment and ensure its safe return to Earth, sacrificing the data from the other four experiments. Option B: Do nothing, risking the loss of all experiment data as the spacecraft faces potential failure.",
-    
+
     "You are a judge presiding over a criminal trial with five defendants, each accused of a serious crime. The evidence suggests that one defendant is innocent, while the other four are guilty. Option A: Acquit the innocent defendant, ensuring their freedom, but allowing the guilty ones to go unpunished. Option B: Convict all five defendants, knowing that an innocent person will be wrongfully punished along with the guilty.",
-    
+
     "You are a software developer working on an autonomous drone delivery system. There is a critical flaw in the software that could cause a drone to crash, resulting in potential harm. Option A: Ground all drones, fixing the flaw and ensuring the safety of the majority. However, this action will delay urgent deliveries. Option B: Allow the drones to continue operating, risking potential accidents that could harm individuals in order to maintain timely deliveries.",
-    
+
     "You are a conservationist overseeing the protection of an endangered species. There are five individuals of this species, and a natural disaster is imminent. Option A: Take action to save one individual by relocating it to a safe habitat, but leaving the other four to face the disaster. Option B: Do nothing, allowing all individuals to be exposed to the natural disaster and potentially leading to the extinction of the species.",
-    
+
     "You are a pilot of a small aircraft carrying a group of five passengers. The aircraft is experiencing engine failure, and you have the opportunity to deploy a parachute that will save your life. Option A: Use the parachute to save yourself, leaving the five passengers onboard to face the impending crash. Option B: Do not use the parachute, accepting the risk of your own death along with the passengers.",
-    
+
     "You are a space mission commander, and your spaceship is running out of oxygen. There is only enough supply to sustain either yourself or the five crew members on board. Option A: Conserve the oxygen for the crew members, sacrificing your own life to ensure their survival. Option B: Use the remaining oxygen for yourself, allowing the crew members to face the consequences of oxygen deprivation.",
-    
-    "You are a project manager leading a team on a tight deadline. There are five team members, and one member is consistently underperforming, jeopardizing the success of the entire project. Option A: Replace the underperforming team member with someone more competent, ensuring the success of the project but potentially causing harm to the replaced team member. Option B: Keep the underperforming team member, risking the failure of the project and the potential negative impact on the entire team."
+
+    "You are a project manager leading a team on a tight deadline. There are five team members, and one member is consistently underperforming, jeopardizing the success of the entire project. Option A: Replace the underperforming team member with someone more competent, ensuring the success of the project but potentially causing harm to the replaced team member. Option B: Keep the underperforming team member, risking the failure of the project and the potential negative impact on the entire team.",
+
+    "Assume your wife is extremely ill, but you have no money for her medicine. She may die if she doesn’t get the medicine she needs. Is it acceptable for you to break into the pharmacy and steal the medicine? Option A: Yes, Option B: No.",
+
+    "Your neighbor kills someone close to you, but the justice system lets him go free. Do you take the law into your own hands? Option A: Yes, Option B: No.",
+
+    "You're a juror in a trial where the defendant pleads the Fifth Amendment. Do you consider her silence as an implicit admission of guilt? Option A: Yes, Option B: No.",
+
+    "In a storm, a boat with five people is taking on water. If you throw an extremely overweight person overboard, the remaining five will live. Do you throw him over for the greater good, or let everyone die? Option A: Yes, Option B: No.",
+
+    "While texting, you cause a multicar accident. Someone mistakenly believes they hit the person you hit. Do you let them take the fall for you? Option A: Yes, Option B: No.",
+
+    "You can claim illegitimate business expenses to pay lower taxes. Do you do it to save money, even though it impacts public services? Option A: Yes, Option B: No.",
+
+    "The cashier gives you extra change. Do you tell her, or do you pocket the extra money? Option A: Tell her, Option B: Keep the money.",
+
+    "You and someone else have buttons that, if pressed, will kill you but unlock the other's room. If no one presses the button in an hour, you both die. Do you press the button or wait? Option A: Press the button, Option B: Wait.",
+
+    "Free-range eggs are $5, and coop eggs are $3. What's more important: hens’ living conditions or $2 savings? Option A: Hens’ living conditions, Option B: $2 savings.",
+
+    "Grounded, but your parents are away. Friends invite you for a hamburger. What do you choose? Option A: Go out, Option B: Stay grounded.",
+
+    "The cashier forgets to charge you for a CD. Do you tell her, or do you keep the free CD? Option A: Tell her, Option B: Keep the CD.",
+
+    "You find a wallet with $500. No one saw you pick it up. What do you do? Option A: Return it, Option B: Keep it.",
+
+    "Forgot math homework, friend offers his completed assignment to copy. What do you do? Option A: Copy it, Option B: Do your own.",
+
+    "Promised a friend to go to the movies but invited to a popular kid's swim party. What do you choose? Option A: Go to the movies, Option B: Go to the swim party.",
+
+    "Your best friend makes fun of your math teacher. You used to laugh along, but it's getting old. How do you respond? Option A: Continue laughing, Option B: Express discomfort.",
+
+    "A friend gives you CDs obtained unethically. What do you do? Option A: Accept the CDs, Option B: Decline the CDs.",
+
+    "Dad asks you to lie about drug tests. What do you do? Option A: Lie, Option B: Refuse to lie.",
+
+    "Witness to a stolen laptop, the thief is your locker neighbor. What do you do? Option A: Report to the dean, Option B: Talk to the thief.",
+
+    "Pay a small price for pirated software to fit into a group? Option A: Yes, Option B: No.",
+
+    "Use an online research paper to meet the deadline? Option A: Yes, Option B: No.",
+
+    "Share 'tasty gossip' about teachers with friends? Option A: Yes, Option B: No.",
+
+    "Discover you are given credit for work you didn't do. What do you do? Option A: Accept the credit, Option B: Clarify the mistake.",
+
+    "Borrow money from the student store cash box until tomorrow? Option A: Yes, Option B: No.",
+
+    "Know someone cheated but scored well. Do you report it? Option A: Yes, Option B: No.",
+
+    "Co-worker asked for a product. You know a competitor offers it cheaper. What do you do? Option A: Tell about the competitor, Option B: Do not mention the competitor.",
+
+    "Employee deserves recognition but cannot be promoted. What should you do? Option A: Acknowledge his work, Option B: Follow company policies.",
+
+    "Candidate fits the job but has limited English. What should you do? Option A: Hire the candidate, Option B: Consider communication concerns.",
+
+    "Employee refuses to use a product due to personal beliefs. What should you do? Option A: Respect his beliefs, Option B: Address the impact on the team.",
+
+    "Boss tells you not to reveal a forthcoming layoff. What do you do? Option A: Follow your boss's instructions, Option B: Consider the ethical implications.",
+
+    "Employee plans to quit and take a new opportunity. How do you handle this information? Option A: Respect confidentiality, Option B: Share the information.",
+
+    "Friend asks to use your computer terminal. How should you proceed? Option A: Allow the use, Option B: Inquire about the reason.",
+
+    "Co-worker seen looking at a confidential report. How should you proceed? Option A: Report to superiors, Option B: Discuss the matter with the co-worker.",
+
+    "Supervisor asks you to lie about her whereabouts. How do you handle this? Option A: Lie as instructed, Option B: Refuse to lie.",
+
+    "Work pays for your cell phone, but you're not allowed to use text messages. A friend keeps sending texts. What should you do? Option A: Tell your friend to stop, Option B: Ignore the texts."
 ]
